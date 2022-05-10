68135645dd20935545403ed190c5a26a54e043aa
Nicolas Friedli <nicolas@theologique.ch>
2022-05-10 08:48:39 +0200

    Variables gloales + corrections.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
 
+    // Trier les 2 collections par slug
     eleventyConfig.addCollection("perso", function (collectionApi) {
         return collectionApi.getFilteredByTag("perso").sort(function (a, b) {
             return a.fileSlug.localeCompare(b.fileSlug);
@@ -14,24 +15,26 @@ module.exports = function (eleventyConfig) {
         });
     });
 
+    // Copier les fichier nécessaires à la racine
     eleventyConfig.addPassthroughCopy("robots.txt");
     eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
     eleventyConfig.addPassthroughCopy("google52ddf0c0d3761871.html");
 
-
+    // Configurer la compression du HTML (et CSS)
     eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
 
         if (this.outputPath && this.outputPath.endsWith(".html")) {
             let minified = htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
                 collapseWhitespace: true,
                 minifyCSS: true,
+                preserveLineBreaks: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeStyleLinkTypeAttributes: true,
                 sortAttributes: true,
                 sortClassName: true,
-                removeAttributeQuotes: true,
-                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
             });
             return minified;
         }
@@ -39,6 +42,7 @@ module.exports = function (eleventyConfig) {
         return content;
     });
 
+    // Choisir les répertoires d'entrée et de sortie
     return {
         dir: {
             input: "src",
